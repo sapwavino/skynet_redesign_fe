d711281027c2b980df0faffb68c9b7343c5a0492
sapwavino <sapwavino@protonmail.com>
2025-03-26 10:02:50 +0100

    ADDED INVOICE VIEW PAGE

---
+++ src/components/DashboardInvoice.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <section>
+    <button class="btn-base-sm"
+            @click.prevent="$router.back()"
+    >← Back
+    </button>
+  </section>
+  <div class="dark:text-gray-300">This is for {{ $route.params.id }}</div>
+</template>
+
+<style scoped>
+
+</style>
